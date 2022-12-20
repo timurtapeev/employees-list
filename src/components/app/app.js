2b8606c31be3c9f4ae600cfe7c9cf5297273cb73
tapeevtimur <tapeevtimur@mail.ru>
2022-12-20 17:48:38 +0500

    search-panel: employee search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,6 +16,7 @@ class App extends Component {
                 {name: 'Alex Smith', salary: 500, increase: true, rise: false, id: 2},
                 {name: 'Ivan Duglas', salary: 1100, increase: false, rise: false, id: 3}
             ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -55,21 +56,36 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term: term});
+    }
+
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.searchEmp(data, term);
+        
         return (
             <div className="app">
-                <AppInfo
-                    employees={employees}
-                    increased={increased}/>
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={id => this.deleteItem(id)}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm
